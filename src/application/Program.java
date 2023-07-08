2d568b4a675b42f6fc17c862be546eb5cb1b49bc
Bianca <bianca-reis2001@hotmail.com>
2023-07-08 12:36:20 -0300

    Exercise 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,14 @@
 package application;
 
-import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//Exercicio 1
 		String str = "Hello, World! OpenAI is amazing.";
 		
 		String[] pilha = str.split(" ");
@@ -14,7 +16,19 @@ public class Program {
 		for(int i = pilha.length - 1; i >= 0; i--) {
 			System.out.print(pilha[i] + " ");
 		}
+		System.out.println();
 		
-	}
+		//Exercicio 2
+		String str2 = "Hello, World!";
+		char[] ch = str2.toCharArray();
+		
+		Set<Character> set = new LinkedHashSet<>();
+		int len = 0;
+		for(char c : ch) {
+			if(set.add(c))
+				ch[len++] = c;
+		}
+		System.out.println(new String(ch, 0, len));
 
+	}
 }

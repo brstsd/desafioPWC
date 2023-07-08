1cb2eef9a60822bf245bb2ce34a1352e0073fe46
Bianca <bianca-reis2001@hotmail.com>
2023-07-08 11:20:43 -0300

    Exercise 1

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,20 @@
 package application;
 
+import java.util.Iterator;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String str = "Hello, World! OpenAI is amazing.";
+		
+		String[] pilha = str.split(" ");
+		
+		for(int i = pilha.length - 1; i >= 0; i--) {
+			System.out.print(pilha[i] + " ");
+		}
+		
 	}
 
 }
